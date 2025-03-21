f753c4ca99317527a185bd8cdcee2e96a4ed7c07
thisaramax20 <thisarathejana20@gmail.com>
2025-03-21 15:11:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="bg-red-100">
+      <Button>Click</Button>
+    </div>
+  );
+};
+
+export default App;
